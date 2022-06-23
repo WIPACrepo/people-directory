c44e888bee8aff83580a2f14b77a468146b4b96d
David Schultz <davids24@gmail.com>
2022-06-23 15:29:39 -0500

    add new ci workflows and dependencies (#1)
    * add new ci workflows and dependencies
    
    * <bot> update setup.cfg
    
    * <bot> update README.md
    
    * <bot> update requirements.txt
    
    * remove old db code
    
    * fix flake8 errors
    
    Co-authored-by: github-actions <github-actions@github.com>

--- people_directory/server.py
+++ people_directory/server.py
@@ -3,20 +3,21 @@ Server for people directory
 """
 
 from datetime import datetime, timedelta
-from functools import partial
 import json
 import logging
 import re
 import os
 
-from tornado.web import RequestHandler, HTTPError
-from tornado.escape import xhtml_escape
-from rest_tools.server import RestServer, from_environment
+from tornado.web import RequestHandler
+from rest_tools.server import RestServer
+from wipac_dev_tools import from_environment
 
 from .people import People
 
 
 CLEANR = re.compile('<.*?>')
+
+
 def recursive_escape(data):
     if isinstance(data, dict):
         return {recursive_escape(k): recursive_escape(v) for k,v in data.items()}
@@ -24,6 +25,7 @@ def recursive_escape(data):
         return [recursive_escape(v) for v in data]
     else:
         return re.sub(CLEANR, '', data)
+
 
 def escape_json(data, key=None):
     ret = {} if key else []
@@ -34,8 +36,9 @@ def escape_json(data, key=None):
         if key:
             ret[item[key]] = item
         else:
-            ret.append(val)
+            ret.append(item)
     return ret
+
 
 class Main(RequestHandler):
     def initialize(self, people):
@@ -50,6 +53,7 @@ class Main(RequestHandler):
                 logging.info(f'{u}')
         self.render('index.html', json=json, insts=insts, users=users)
 
+
 class Health(RequestHandler):
     def initialize(self, people):
         self.people = people
@@ -62,6 +66,7 @@ class Health(RequestHandler):
         if (not self.people.last_update) or datetime.utcnow() - self.people.last_update > timedelta(hours=1):
             self.set_status(400)
 
+
 def create_server():
     static_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
 
@@ -72,10 +77,6 @@ def create_server():
         'EXPERIMENT': 'IceCube',
     }
     config = from_environment(default_config)
-
-    rest_config = {
-        'debug': config['DEBUG'],
-    }
 
     kwargs = {}
     kwargs['people'] = People(config['EXPERIMENT'])
--- setupenv.sh
+++ setupenv.sh
@@ -3,4 +3,4 @@ unset PYTHONPATH
 python3 -m virtualenv -p python3 env
 echo "unset PYTHONPATH" >> env/bin/activate
 . env/bin/activate
-pip install -r requirements.txt
+pip install -e .[tests]
